d0aa3fdb35bab72e1a670b231682578c464a39be
Mitesh <miteshtagadiya@gmail.com>
2019-04-23 11:11:03 +0530

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+require('./styles.css');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var MyComponent = function MyComponent() {
+    return _react2.default.createElement(
+        'h1',
+        null,
+        'Hello from My Component'
+    );
+};
+exports.default = MyComponent;
